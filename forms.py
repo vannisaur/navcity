0499cfcb0ffa7e87e5261a7e4db6dbc7050bb7cd
Vanni <bielvanni@hotmail.com>
2021-02-16 03:13:55 -0300

    First Commit

---
+++ forms.py
@@ -0,0 +1,10 @@
+"""Formularios para primeiro Site"""
+
+from django.forms import ModelForm
+from app.models import Carros
+
+# Create the form class.
+class CarrosForm(ModelForm):
+    class Meta:
+        model = Carros
+        fields = ['modelo', 'marca', 'ano']
